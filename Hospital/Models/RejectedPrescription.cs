2ffdacaac621d8a1a9acb16f92be2c0278ced3c1
Nikita-Davids <101563505+Nikita-Davids@users.noreply.github.com>
2024-08-28 14:55:02 +0200

    pull attempt

---
+++ Hospital/Models/RejectedPrescription.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital.Models
+{
+    public class RejectedPrescription
+    {
+        [Key]
+        public int RejectionId { get; set; }
+
+        [Required]
+        public int PrescriptionId { get; set; }
+
+        [Required]
+        public int SurgeonId { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 1)]
+        public string RejectionReason { get; set; }
+
+        [Required]
+        public DateTime RejectionDate { get; set; } = DateTime.Now; // Default value set to the current date and time
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = "Pending";
+
+        public string PharmacistName { get; set; }
+
+        public string PharmacistSurname { get; set; }
+
+        //Navigation properties
+       [ForeignKey("PrescriptionId")]
+        public virtual SurgeonPrescription SurgeonPrescription { get; set; }
+
+        [ForeignKey("SurgeonId")]
+        public virtual Surgeon Surgeon { get; set; }
+    }
+}
