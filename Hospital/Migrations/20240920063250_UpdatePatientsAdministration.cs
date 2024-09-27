58fc74d2ce0e03e0dff5b530e23835fed4adf084
Gabriel Kyereahene Mensah <s229049206@mandela.ac.za>
2024-09-27 07:48:40 +0200

    migratioins push

---
+++ Hospital/Migrations/20240920063250_UpdatePatientsAdministration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePatientsAdministration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PatientWard",
+                table: "PatientsAdministration",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PatientBed",
+                table: "PatientsAdministration",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAssigned",
+                table: "PatientsAdministration",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAssigned",
+                table: "PatientsAdministration");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PatientWard",
+                table: "PatientsAdministration",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PatientBed",
+                table: "PatientsAdministration",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
