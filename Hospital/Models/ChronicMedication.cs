2f08337c07de44228793b693077ec51fb76e0a4a
Nikita-Davids <101563505+Nikita-Davids@users.noreply.github.com>
2024-10-18 10:20:27 +0200

    added model

---
+++ Hospital/Models/ChronicMedication.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital.Models
+{
+    public class ChronicMedication
+    {      
+            [Key]
+            public int ChronicMedicationId { get; set; }
+
+
+            [Required]
+            [Display(Name = "Chronic Medication Name")]
+            public string CMedicationName { get; set; }
+
+
+            [Required]
+            [Display(Name = "Dosage Form")]
+            public string CDosageForm { get; set; }
+
+
+            [Required]
+            [Display(Name = "Schedule")]
+            public string CSchedule { get; set; }
+
+
+            [Required]
+            [Display(Name = "Medication Active Ingredients")]
+            public string CMedicationActiveIngredients { get; set; }
+           
+        }
+}
